efdf62fbf54ede80451332ce1154e9a4171a0c37
chunnu <chunnukumaryadav25@gmail.com>
2026-03-15 09:05:46 +0530

    initialised

--- 02_embeddings.py
+++ 02_embeddings.py
@@ -1,4 +1,13 @@
 from dotenv import load_dotenv
-from openai import OpenAI
-
+from google import genai
+from google.genai import types
 load_dotenv()
+
+client = genai.Client()
+text = "Eiffel Tower is in Paris and is a famous landmark, it is 324 meters tall"
+
+response= client.models.embed_content(
+contents= text,
+model="gemini-embedding-001"
+)
+print("Vector Embeddings:", response.embeddings[0].values)
--- 03_gemini_api.py
+++ 03_gemini_api.py
@@ -1,6 +1,9 @@
 from google import genai
 from google.genai import types
-client = genai.Client(api_key='AIzaSyCTY-6evaMbWR_e0whGTAfLuOc4CI9AoIY')
+from dotenv import load_dotenv
+load_dotenv()
+
+client = genai.Client()
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents='Why is the sky blue?'
 )
